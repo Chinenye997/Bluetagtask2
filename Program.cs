85798591524d9c099861ba068e2f4e573288ff17
chinenye997 <edehg997@gmail.com>
2024-08-26 22:53:10 +0100

    Task2 Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluetagtask2
+{
+    class AreaofCircle
+    {
+        static void Main(string[] args)
+        {
+
+            double radius, area;
+            Console.WriteLine("Enter the radius of the circle");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            area = Math.PI * radius * radius;
+
+            Console.WriteLine("Area of the circle with radius {0} is {1}", radius, area);
+
+            Console.WriteLine("Press any key to exit");
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
